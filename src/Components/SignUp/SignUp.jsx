f4ab69312dacf37fefab0bcc7278624b870cfc57
Emon <emon.mhk69@gmail.com>
2023-04-17 00:25:20 +0600

    login and signup done

--- src/Components/SignUp/SignUp.jsx
+++ src/Components/SignUp/SignUp.jsx
@@ -1,10 +1,49 @@
 import React from 'react';
-import "./SignUp.css"
+import "./../LogIn/Login.css"
+import { Link } from 'react-router-dom';
 
 const SignUp = () => {
     return (
-        <div>
-            <h2>this is sign up page</h2>
+        <div className='container'>
+            <div className="form-container">
+
+                <h2 className='form-title'>Sign Up</h2>
+
+                <form className='form'>
+                    <div className="form-controler">
+                        <label htmlFor="email">Email</label>
+                        <input type="email" name='email' id='email' required/>
+                    </div>
+                    <div className="form-controler">
+                        <label htmlFor="password">Password</label>
+                        <input type="password" name='password' id='password' required/>
+                    </div>
+                    <div className="form-controler">
+                        <label htmlFor="password">Confirm Password</label>
+                        <input type="password" name='password' id='password' required/>
+                    </div>
+                    <button className='login-btn'>
+                        Sign Up
+                    </button>
+
+                    <p className='link-text'>
+                        Already Have an account ? <Link to="/login">Login</Link>
+                    </p>
+
+                    <div className="options">
+                        <p>----------------</p>
+                        <p>OR</p>
+                        <p>----------------</p>
+                    </div>
+
+                </form>
+
+                <p className='social-login'>
+                    <img className='google-logo' src="https://static.vecteezy.com/system/resources/previews/010/353/285/original/colourful-google-logo-on-white-background-free-vector.jpg" alt="" />
+                    <span>Continue With Google</span>
+                </p>
+
+            </div>
         </div>
     );
 };
